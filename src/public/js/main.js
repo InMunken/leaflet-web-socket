4f9f57aa584eb708a049679f94a54aff76b7ad98
InMunken <FedericoDaRosa137682@gmail.com>
2024-02-07 10:58:15 -0300

    add button to share location

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,28 +1,32 @@
+//this is the client.
+
 const map = L.map('map-template').setView([-34.572267, -58.439947], 11);
 
 const socket = io();
+
+const boton = document.getElementById("burrrron")
+
+let locationData;
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 map.locate({ enableHighAccuracy: true })
 map.on('locationfound', e => {
-
+    locationData = e;
     const marker = L.marker([e.latlng.lat, e.latlng.lng]);
-
     marker.bindPopup('Soy yo el que está acá');
-
     marker.addTo(map);
-
     socket.emit('coordinadasUsuario', e.latlng)
-
 })
 
 socket.on('usuarioConectado', (coords, userinformation) =>{
-
     const marker = L.marker([(coords.lat), coords.lng]);
-
     marker.bindPopup(userinformation);
-
     marker.addTo(map);
- 
 });
+
+boton.onclick = function() {
+    console.log(locationData)
+    socket.emit('coordinadasUsuario', locationData.latlng)
+
+}
