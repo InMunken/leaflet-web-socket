c69c1b6d8ab007f4f224d2ff978593f52d9f27c9
InMunken <FedericoDaRosa137682@gmail.com>
2024-02-07 10:12:50 -0300

    behavior on connect changed

--- src/sockets.js
+++ src/sockets.js
@@ -1,6 +1,12 @@
 module.exports = io => {
     io.on('connection', (socket) => {
-        console.log("alguien se conectó!")
+        let userinformation = socket.request.connection.remoteAddress
+
+        console.log("alguien se conectó!: ", userinformation )
+
+        socket.on('disconnect', () =>{
+            console.log("alguien se desconectó! :( : ", userinformation)
+        })
 
         socket.on('coordinadasUsuario', coords => {
             socket.broadcast.emit('usuarioConectado', coords)
