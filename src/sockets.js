cbde44bffb35f0195709bedd8fec983d5ec78a38
InMunken <FedericoDaRosa137682@gmail.com>
2024-02-08 10:26:11 -0300

    Add user list

--- src/sockets.js
+++ src/sockets.js
@@ -1,17 +1,18 @@
+let fldsmdfr = [];
+
 module.exports = io => {
     io.on('connection', (socket) => {
         let userinformation = socket.request.connection.remoteAddress
-
         console.log("alguien se conectó!: ", userinformation)
-
+            
         socket.on('disconnect', () => {
             console.log("alguien se desconectó! :( : ", userinformation)
         })
         
         socket.on('usuarioActualizado', data => {
-            socket.broadcast.emit('usuarioConectado', {nombre: data.nombre, latlng: data.latlng})
+            fldsmdfr.push(data);
+            io.emit('usuarioConectado', fldsmdfr)
         });
-
     });
 
 }
